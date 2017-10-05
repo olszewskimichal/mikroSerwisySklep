b4ab8ee06ce9c8d846d2af742920997e9f35f34a
Michał <michalolsz@mat.umk.pl>
2017-10-05 22:09:50 +0200

    Testy do repozytorium z produktami

--- productservice/src/main/java/pl/michal/olszewski/repository/ProductRepository.java
+++ productservice/src/main/java/pl/michal/olszewski/repository/ProductRepository.java
@@ -5,13 +5,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import pl.michal.olszewski.entity.Product;
 import pl.michal.olszewski.entity.ProductDefinition;
-
-import java.math.BigDecimal;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
