eef1fade3d69938eeb7536605ba94141546487b5
Michał <michalolsz@mat.umk.pl>
2017-10-08 15:56:38 +0200

    wersjonowanie produktów i ich definicji

--- productservice/src/main/java/pl/michal/olszewski/entity/Product.java
+++ productservice/src/main/java/pl/michal/olszewski/entity/Product.java
@@ -1,7 +1,13 @@
 package pl.michal.olszewski.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.envers.Audited;
+import org.hibernate.envers.NotAudited;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import pl.michal.olszewski.enums.ProductStatus;
 
 import javax.persistence.*;
@@ -9,15 +15,24 @@ import javax.persistence.*;
 @Entity
 @Data
 @NoArgsConstructor
+@Audited
+@EntityListeners(AuditingEntityListener.class)
 public class Product {
     @Id
     @GeneratedValue
     private Long id;
 
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     private ProductDefinition productDefinition;
 
     private Long productStatus;
+
+    @CreatedDate
+    private Long createdAt;
+
+    @LastModifiedDate
+    private Long lastModified;
 
     public Product(ProductDefinition productDefinition, ProductStatus productStatus) {
         this.productDefinition = productDefinition;
---
+++ productservice/src/main/java/pl/michal/olszewski/repository/RevisionRepository.java
@@ -0,0 +1,35 @@
+package pl.michal.olszewski.repository;
+
+import org.hibernate.envers.AuditReader;
+import org.hibernate.envers.AuditReaderFactory;
+import org.springframework.stereotype.Repository;
+import pl.michal.olszewski.entity.EntityWithRevision;
+import pl.michal.olszewski.entity.RevisionsEntity;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Repository
+@Transactional
+public class RevisionRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    public List<EntityWithRevision> getEntityRevision(Long id,Class aClass) {
+        AuditReader auditReader = AuditReaderFactory.get(entityManager);
+        List<Number> revisions = auditReader.getRevisions(aClass, id);
+
+        List<EntityWithRevision> entityRevision = new ArrayList<>();
+        for (Number revision : revisions) {
+            Date revisionDate = auditReader.getRevisionDate(revision);
+            entityRevision.add(new EntityWithRevision(new RevisionsEntity(revision.longValue(), revisionDate), auditReader.find(aClass, id, revision)));
+        }
+        return entityRevision;
+    }
+}
